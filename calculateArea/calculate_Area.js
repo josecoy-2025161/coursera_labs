0f64bc985dc625e2a74114ec78758e2dd1c2f2a8
josecoy-2025161 <jcoy-2025161@kinal.edu.gt>
2026-06-15 19:20:15 -0600

    calculadora de comestibles js

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -9,3 +9,12 @@ let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
 
+function calcularTotal() {
+ com1 = parseFloat(document.getElementById('comestible1').value);
+ com2 = parseFloat(document.getElementById('comestible2').value);
+ com3 = parseFloat(document.getElementById('comestible3').value);
+
+let totalCompra = com1 + com2 + com3;
+document.getElementById('totalCompra').innerText = `El total de la compra es: $${totalCompra}`;
+}
+
