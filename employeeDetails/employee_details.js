1e4bebce562bdec76761b00e83847fb3b2b95aef
josecoy-2025161 <jcoy-2025161@kinal.edu.gt>
2026-07-07 18:46:38 -0600

    display de array segun especializacion

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,19 +1,19 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Jane Doe', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Java' },
+    { id: 2, name: 'Jane Doe', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'JavaScript' },
     //... Se pueden agregar más registros de empleados aquí
-    { id: 4, name: 'Jane Williams', age: 32, department: 'Marketing', salary: 55000 },
-    { id: 5, name: 'Michael Brown', age: 40, department: 'Sales', salary: 70000 },
-    { id: 6, name: 'Emily Davis', age: 27, department: 'IT', salary: 48000 },
-    { id: 7, name: 'David Wilson', age: 29, department: 'HR', salary: 46000 },
-    { id: 8, name: 'Mac Miller', age: 31, department: 'Finance', salary: 62000 },
+    { id: 4, name: 'Jane Williams', age: 32, department: 'Marketing', salary: 55000, specialization: 'TypeScript' },
+    { id: 5, name: 'Michael Brown', age: 40, department: 'Sales', salary: 70000, specialization: 'Java' },
+    { id: 6, name: 'Emily Davis', age: 27, department: 'IT', salary: 48000, specialization: 'Python' },
+    { id: 7, name: 'David Wilson', age: 29, department: 'HR', salary: 46000, specialization: 'JavaSript' },
+    { id: 8, name: 'Mac Miller', age: 31, department: 'Finance', salary: 62000, specialization: 'Python' },
 ];
 
 // Función para mostrar todos los empleados
 function displayEmployees() {
     const totalEmployees = employees
-        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`)
+        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`)
         .join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
@@ -25,16 +25,22 @@ function calculateTotalSalaries() {
 
 function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department === 'HR');
-    const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }
 
 function findEmployeeById(employeeId) {
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if (foundEmployee) {
-        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specialization}</p>`;
     }
     else {
         document.getElementById('employeesDetails').innerHTML = 'no se ha encontrado ningún empleado con este ID';
     }
+}
+
+function displayBySpecialization(specialization) {
+    const filteredEmployees = employees.filter(employee => employee.specialization === specialization);
+    const employeesDisplay = filteredEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = employeesDisplay;
 }
