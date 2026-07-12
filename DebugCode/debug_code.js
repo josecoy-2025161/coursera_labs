d49549549dc9b3e427684ac2f2f018bffff6fe04
josecoy-2025161 <jcoy-2025161@kinal.edu.gt>
2026-07-11 19:22:43 -0600

    debugging con varias funciones en js

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -6,25 +6,53 @@ function performOperation() {
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
         // Perform the operation
-        let result = multiply(num1, num2);
+        let multiplyOp = multiply(num1, num2);
+        let sumOp = sum(num1, num2);
+        let subtractOp = subtract(num1, num2);
+        let divideOp = divide(num1, num2);
 
-        // Display the result
-        displayResult(result);
+        // Display the results
+        displayResult(`Multiply: ${multiplyOp}, Sum: ${sumOp}, Subtract: ${subtractOp}, Divide: ${divideOp}`);
     } else {
         displayResult('Please enter valid numbers');
     }
 }
 
+function sum(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // Add the numbers
+    return a + b;
+}
+
+function subtract(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // Subtract the numbers
+    return a - b;
+}
+
 function multiply(a, b) {
     // Introduce a debugger statement to pause execution
     debugger;
-
     // Multiply the numbers
     return a * b;
+}
+
+function divide(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // Check for division by zero
+    if (b === 0) {
+        return 'Error: Division by zero';
+    }
+    // Divide the numbers
+    return a / b;
 }
 
 function displayResult(result) {
     // Display the result in the paragraph element
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is: ${result}`;
+    resultElement.textContent = `The result are: 
+    ${result}`;
 }
